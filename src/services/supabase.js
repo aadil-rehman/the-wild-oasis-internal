7bd22fdc63e22f73fb2b18ce2e5dccb8af323dbd
Moh Aadil <aadilrehman.development@gmail.com>
2024-04-22 17:12:56 +0530

    Integrated supabase and created apiCabins to get cabins data

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://gzgotsfnpkhaaglhvzgx.supabase.co";
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6Z290c2ZucGtoYWFnbGh2emd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3MTc0NzEsImV4cCI6MjAyOTI5MzQ3MX0.N69z5nON5IYVBfx9fMyeEkzw9TGWyWKSmMAjc2Tnhug";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
