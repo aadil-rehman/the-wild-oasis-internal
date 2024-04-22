e2385537c8f8633f273a66bd92a697f139565947
Moh Aadil <aadilrehman.development@gmail.com>
2024-04-23 00:43:14 +0530

    Implement cabins data fetching using react-query and add feature to delete a cabin

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Account from "./pages/Account";
@@ -10,9 +13,18 @@ import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 0,
+		},
+	},
+});
+
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools />
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -29,7 +41,7 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 }
 
