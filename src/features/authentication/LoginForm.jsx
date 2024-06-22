0dc2aa07600b91113987b86c60c80813efa198ba
Moh Aadil <aadilrehman.development@gmail.com>
2024-06-22 22:44:30 +0530

    small change

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("aadilrehman@gfail.con");
+	const [password, setPassword] = useState("aadil123");
 	const { login, isLoading } = useLogin();
 
 	function handleSubmit(e) {
